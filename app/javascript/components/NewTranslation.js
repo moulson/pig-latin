dccebc05cf4da5951c7676edc2a7b47e60f81ecc
Daniel Moulson <dan24797@gmail.com>
2019-09-24 17:27:42 +0100

    added form to react component

--- app/javascript/components/NewTranslation.js
+++ app/javascript/components/NewTranslation.js
@@ -8,25 +8,30 @@ class NewTranslation extends React.Component{
             output: props.translation.output
         };
         this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleInputChange(e){
         this.setState({input: e.target.value})
     }
-
+    handleSubmit(e){
+        e.preventDefault;
+    }
     render(){
         return(
-            <div>
-                <label>Text to translate</label>
-                <input
-                    class="form-control"
-                    type="text"
-                    name="translation[input]"
-                    value={this.state.input}
-                    onChange={this.handleInputChange}
-                />
-                <input type="submit" value="Add Translation" class="btn btn-primary" />
-            </div>
+            <form action="/translations" method="post">
+                <div>
+                    <label>Text to translate</label>
+                    <input
+                        class="form-control"
+                        type="text"
+                        name="translation[input]"
+                        value={this.state.input}
+                        onChange={this.handleInputChange}
+                    />
+                    <input type="submit" value="Add Translation" className="btn btn-primary" />
+                </div>
+            </form>
         );
     }
 }
