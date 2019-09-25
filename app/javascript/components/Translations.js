bbaf128309c8a7b6e881046db77eb2ec27c7b810
Daniel Moulson <dan24797@gmail.com>
2019-09-25 14:24:25 +0100

    reactjs files

---
+++ app/javascript/components/Translations.js
@@ -0,0 +1,132 @@
+import React from 'react'
+
+class Translations extends React.Component{
+
+    state = {data: this.props.initialData};
+    loadTranslationsFromServer = () => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', '/translations.json', true);
+        xhr.onload = function(){
+            const data = JSON.parse(xhr.responseText);
+            this.setState({data: data});
+        }.bind(this)
+        xhr.send()
+    };
+    
+    handleTranslationSubmit = translation => {
+        translation.id = Date.now();
+        this.setState({data: translation});
+
+        var data = new FormData();
+        data.append('input', translation.input);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', '/translations.json', true)
+        xhr.onload = function(){
+            this.loadTranslationsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    }
+
+    componentDidMount(){
+        this.loadTranslationsFromServer();
+    }
+
+    render(){
+        return(
+            <div className="translations">
+                <TranslationsTable data={this.state.data} />
+                <TranslationForm onTranslationSubmit={this.handleTranslationSubmit}/>
+            </div>
+        );
+    }
+}
+
+class TranslationsTable extends React.Component{
+    render(){
+        var translations = this.props.data.map(function(translation){
+            console.log(translation);
+            return(
+                <Translation input={translation.input} output={translation.output} id={translation.id} />
+            );
+        })
+        return (
+            <table>
+                {translations}
+            </table>
+        );
+    }
+}
+
+class TranslationForm extends React.Component{
+    state = {
+        input: ''
+    };
+
+    handleInputChange = e => {
+        this.setState({input: e.target.value});
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        var input = this.state.input.trim();
+        if(!input){
+            return;
+        }
+        this.props.onTranslationSubmit({input: input});
+        this.setState({input: ''});
+    };
+    render(){
+        return(
+            <form 
+                acceptCharset="UTF-8"
+                onSubmit={this.handleSubmit}
+            >
+                <input
+                    name="utf8"
+                    type="hidden"
+                    value="✓"
+                />
+                <input
+                    type="text"
+                    className="form-control"
+                    name="translation[input]"
+                />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Add Translation"
+                />
+            </form>
+        );
+    }
+}
+
+class Translation extends React.Component{
+    render(){
+        console.log("translation");
+        return(
+            <tr>
+                <td>{this.props.input}</td>
+                <td>{this.props.output}</td>
+                <td>
+                    <div className="btn-group">
+                        <a href={`/translations/${this.props.id}`} className="btn btn-default">Show</a>
+                        <a href={`/translations/${this.props.id}/edit`} className="btn btn-default">Edit</a>
+                        <a 
+                            href={`/translations/${this.props.id}`} 
+                            className="btn btn-default"
+                            rel="nofollow"
+                            data-confirm="Are you sure?"
+                            data-method="delete"
+                        >
+                            Delete
+                        </a>
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+}  
+
+export default Translations;
